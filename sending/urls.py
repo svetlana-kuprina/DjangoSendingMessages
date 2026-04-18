5a9244bbf244d239b4c11036a45d6dd9375b31f0
Kuprina Svetlana <kupsveta@rambler.ru>
2026-04-18 22:37:47 +0500

    add CRUD

--- sending/urls.py
+++ sending/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from sending.apps import SendingConfig
-from sending.views import SendingMessagesListView
+from sending.views import SendingMessagesListView, ClientListView, MessageListView, SendingMessagesDetailView, \
+    HomeListView, ClientDetailView, ClCreateView
 
 app_name = SendingConfig.name
 
-
-urlpatterns = [path("", SendingMessagesListView.as_view(), name="home"),]
+urlpatterns = [
+    path("", HomeListView.as_view(), name="home"),
+    path("sending_messages/", SendingMessagesListView.as_view(), name="sending_messages"),
+    path("sending_message/<int:pk>/", SendingMessagesDetailView.as_view(), name="sending_message"),
+    path("clients/", ClientListView.as_view(), name="clients"),
+    path("client/<int:pk>/", ClientDetailView.as_view(), name="client"),
+    path("client_сreate/<int:pk>/", ClCreateView.as_view(), name="client_create"),
+    path("messages/", MessageListView.as_view(), name="messages"),
+]
--- sending/views.py
+++ sending/views.py
@@ -1,16 +1,63 @@
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
-from django.shortcuts import render
-
-from sending.models import SendingMessages
 
 
-class SendingMessagesListView(ListView):
+from sending.models import SendingMessages, Client, Message
+
+class HomeListView(ListView):
     model = SendingMessages
     template_name = "home.html"
     context_object_name = "sending_messages"
 
+    def get_queryset(self):
+        """подсчет списка"""
+        queryset = super().get_queryset()
+        if not self.request.session.get('messages_list_viewed', False):
+            self.request.session['messages_list_viewed'] = True
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """Подсчет списка со статусом "launched" и добавление значения в контекст """
+        context = super().get_context_data(**kwargs)
+        context['launched_count'] = self.object_list.filter(status="launched").count()
+        context['total_clients_count'] = Client.objects.count()
+        return context
+
+
+class SendingMessagesListView(ListView):
+    model = SendingMessages
+    template_name = "sending_messages.html"
+    context_object_name = "sending_messages"
+
+
+
+
 class SendingMessagesDetailView(DetailView):
     model = SendingMessages
-    template_name = "home.html"
+    template_name = "sending_message.html"
     context_object_name = "sending_message"
 
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = "clients.html"
+    context_object_name = "clients"
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "client.html"
+    context_object_name = "client"
+
+class ClCreateView(CreateView):
+    model = Client
+    template_name = "client_form.html"
+    context_object_name = "client"
+
+
+
+class MessageListView(ListView):
+    model = Message
+    template_name = "messages.html"
+    context_object_name = "messages"
